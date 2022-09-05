881b60998e80620c906071fc30acdc4148ec9960
Shakur <gall.abdulshakur@gmail.com>
2022-09-05 04:19:33 +0100

    navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Summies Blog
+              Shakur Corner
             </span>
           </Link>
         </div>
