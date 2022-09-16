8183b5f1fa9266274ccc7bcd8e1640df9ab2c997
Galla Abdulshakur <56418342+MetaphorX@users.noreply.github.com>
2022-09-17 00:14:48 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Summie&aposs Blog
+              Summie &apos s Blog
             </span>
           </Link>
         </div>
